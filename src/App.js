51c50b8dfa030532e0f07a2a29f0d48ca9a6ffdc
AshwinCarvalho999 <ashwincarvalho27@gmail.com>
2023-02-24 15:59:44 +0100

    created the design for navbar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+// import { Route, Routes } from 'react-router';
+import logo from "../src/images/vespa-logo.png";
+import "./App.css";
+import Navbar from "./components/side-nav";
+import MenuClose from "./components/menu-close";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="navlogo">
+        <img src={logo} width={175} height={100} />
+      </div>
+      <div className="App">
+        <Navbar />
+      
+      <div className="menu-close">
+        <MenuClose />
+      </div>
+      </div>
+    </>
   );
 }
 
---
+++ src/components/menu-close.js
@@ -0,0 +1,12 @@
+import React from "react";
+import CloseMenuIcon from "/media/ashwin/DATA/Main Projects/MotoMate-Frontend/src/images/menu-close.png";
+
+const MenuClose = () => {
+  return (
+    <>
+      <img src={CloseMenuIcon} />
+    </>
+  );
+}
+
+export default MenuClose;
