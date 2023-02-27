d3da284bdf6b5eb61ccd20b0dcbdf604186f14a6
AshwinCarvalho999 <ashwincarvalho27@gmail.com>
2023-02-27 16:08:12 +0530

    trying to install FA

--- src/components/social-icons.js
+++ src/components/social-icons.js
@@ -1,30 +1,11 @@
 import React from "react";
-import { FontAwesomeIcon } from '@fontawesome/react-fontawesome';
-import { faTwitter } from '@fontawesome/free-brands-svg-icons';
-import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
-import { faFacebookSquare } from '@fortawesome/free-brands-svg-icons';
-import { faGoogle } from '@fortawesome/free-brands-svg-icons';
-import { faPinterest } from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 
 const SocialIcons = () => (
-  <>
-  <div className="fa-icon">
-    <FontAwesomeIcon icon={faTwitter} />
+  <div>
+     <FontAwesomeIcon icon="fa-brands fa-twitter" />
   </div>
-  <div className="fa-icon">
-    <FontAwesomeIcon icon={faLinkedin} />
-  </div>
-  <div className="fa-icon">
-    <FontAwesomeIcon icon={faFacebookSquare} />
-  </div>
-  <div className="fa-icon">
-    <FontAwesomeIcon icon={faGoogle} />
-  </div>
-  <div className="fa-icon">
-    <FontAwesomeIcon icon={faPinterest} />
-  </div>
-  </>
 );
 
 export default SocialIcons;
