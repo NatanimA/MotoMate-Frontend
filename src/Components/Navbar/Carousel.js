3fd54fb34be42bd2b382e2ec4167a25a7a19c38a
AdnanOlarmmi <olarmmi@gmail.com>
2023-02-27 23:00:12 +0100

    Add array of objects for car carousel

--- src/Components/Navbar/Carousel.js
+++ src/Components/Navbar/Carousel.js
@@ -4,6 +4,22 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 // Import Swiper styles
 import 'swiper/css';
 
+const cars = [
+    {
+        id: 1,
+        image: "https://images.unsplash.com/photo-1617531653332-bd46c24f2068?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=915&q=80",
+        financeFee: "$129",
+        purchaseFee: "$129",
+        total: "$258",
+        duration: "36 months",
+        deposit: "$100",
+        model: "BMW 3 Series",
+        year: 2021,
+        info: "The BMW 3 Series is a compact executive car manufactured by the German automaker BMW since May 1975. It is the successor to the 02 Series and has been produced in six different generations."
+
+    }
+]
+
 const Carousel = () => {
     return (
       <Swiper
@@ -12,10 +28,7 @@ const Carousel = () => {
         onSlideChange={() => console.log('slide change')}
         onSwiper={(swiper) => console.log(swiper)}
       >
-        <SwiperSlide>Slide 1</SwiperSlide>
-        <SwiperSlide>Slide 2</SwiperSlide>
-        <SwiperSlide>Slide 3</SwiperSlide>
-        <SwiperSlide>Slide 4</SwiperSlide>
+        
       </Swiper>
     );
   };
