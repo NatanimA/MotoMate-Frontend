b608243575b9dd512c0034868e6dcda611805cfc
AdnanOlarmmi <olarmmi@gmail.com>
2023-03-01 22:32:01 +0100

    Complete Signup page

--- src/Components/Signup.js
+++ src/Components/Signup.js
@@ -22,15 +22,15 @@ const Signup = () => {
       <div className="flex justify-between gap-3">
         <span className="w-1/2">
           <label htmlFor="firstname" className="block text-xs font-semibold text-gray-600 uppercase">Firstname</label>
-          <input id="firstname" type="text" name="firstname" placeholder="John" autoComplete="given-name" className="block w-full p-3 mt-2 text-gray-700 bg-gray-200 appearance-none focus:outline-none focus:bg-gray-300 focus:shadow-inner" required />
+          <input id="firstname" type="text" name="firstname" placeholder="John" autoComplete="given-name" className="block w-full p-3 mt-2 text-gray-700 bg-gray-200 appearance-none focus:outline-none focus:bg-gray-300 focus:shadow-inner" value={details.firstname} onChange={handleChange} required />
         </span>
         <span className="w-1/2">
           <label htmlFor="lastname" className="block text-xs font-semibold text-gray-600 uppercase">Lastname</label>
-        <input id="lastname" type="text" name="lastname" placeholder="Doe" autoComplete="family-name" className="block w-full p-3 mt-2 text-gray-700 bg-gray-200 appearance-none focus:outline-none focus:bg-gray-300 focus:shadow-inner" required />
+        <input id="lastname" type="text" name="lastname" placeholder="Doe" autoComplete="family-name" className="block w-full p-3 mt-2 text-gray-700 bg-gray-200 appearance-none focus:outline-none focus:bg-gray-300 focus:shadow-inner" value={details.lastname} onChange={handleChange} required />
         </span>
       </div>
-      <label htmlFor="email" className="block mt-2 text-xs font-semibold text-gray-600 uppercase">Username</label>
-      <input id="username" type="text" name="username" placeholder="username" autoComplete="email" className="block w-full p-3 mt-2 text-gray-700 bg-gray-200 appearance-none focus:outline-none focus:bg-gray-300 focus:shadow-inner" required />
+      <label htmlFor="username" className="block mt-2 text-xs font-semibold text-gray-600 uppercase">Username</label>
+      <input id="username" type="text" name="username" placeholder="username" autoComplete="email" className="block w-full p-3 mt-2 text-gray-700 bg-gray-200 appearance-none focus:outline-none focus:bg-gray-300 focus:shadow-inner" value={details.username} onChange={handleChange} required />
       <button type="submit" className="w-full py-3 mt-6 font-medium tracking-widest text-white uppercase bg-black shadow-lg focus:outline-none hover:bg-gray-900 hover:shadow-none">
         Sign up
       </button>
