caff30caf884155e846a3c3d23a56bf307c610fe
AdnanOlarmmi <olarmmi@gmail.com>
2023-03-01 07:05:44 +0100

    Add dummy data to details page

--- src/Components/Details.js
+++ src/Components/Details.js
@@ -5,11 +5,11 @@ const cardetails =
     {
         id: 1,
         image: "https://images.unsplash.com/photo-1617531653332-bd46c24f2068?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=915&q=80",
-        financeFee: "$129",
-        purchaseFee: "$129",
-        total: "$258",
-        duration: "36 months",
-        deposit: "$100",
+        financeFee: 129,
+        purchaseFee: 129,
+        total: 258,
+        duration: 36,
+        deposit: 100,
         model: "BMW 3 Series",
         year: 2021,
         info: "The BMW 3 Series is a compact executive car manufactured by the German automaker BMW since May 1975."
@@ -25,7 +25,7 @@ const Details = () => {
                 </div>
                 <div className='carinfo'>
                     <h2>{cardetails.model}</h2>
-                    <p>{cardetails.deposit} deposit upon any Moto Purchase</p>
+                    <p>${cardetails.deposit} deposit upon any Moto Purchase</p>
                     <div className="flex flex-col">
                         <div className="overflow-x-auto sm:mx-0.5 lg:mx-0.5">
                             <div className="py-2 inline-block min-w-full sm:px-6 lg:px-8">
@@ -34,39 +34,34 @@ const Details = () => {
                                         <tbody>
                                             <tr className="bg-gray-100 border-b">
                                                 <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                                                    Mark
+                                                    Finance Fee
                                                 </td>
                                                 <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                                                    Otto
+                                                ${cardetails.financeFee}
                                                 </td>
                                             </tr>
                                             <tr className="bg-white border-b">
                                                 <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                                                    Jacob
+                                                    Option to purchase fee
                                                 </td>
                                                 <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                                                    Dillan
+                                                    ${cardetails.purchaseFee}
                                                 </td>
                                             </tr>
                                             <tr className="bg-gray-100 border-b">
                                                 <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                                                    Mark
+                                                    Total amount payable
                                                 </td>
                                                 <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                                                    Twen
+                                                    ${cardetails.total}
                                                 </td>
                                             </tr>    
                                             <tr className="bg-white border-b">
                                                 <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                                                    Bob
+                                                    Duration
                                                 </td>
                                                 <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                                                    Dillan
-                                                </td>
-                                            </tr>
-                                            <tr className="bg-gray-100 border-b">
-                                                <td colSpan="2" className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap text-center">
-                                                    Larry the Bird
+                                                    {cardetails.duration} Months
                                                 </td>
                                             </tr>
                                         </tbody>
