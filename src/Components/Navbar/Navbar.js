9afb7f1f7e47b19188a26bcc13d698c2b9995773
AdnanOlarmmi <olarmmi@gmail.com>
2023-03-04 15:00:46 +0100

    Correct Image paths

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect, useRef } from 'react';
 import { IoMdMenu, IoMdClose } from 'react-icons/io';
 import NavLinks from './NavLinks';
 import SocialLinks from './SocialLinks';
-import Logo from '../../Images/logo.png';
+import Logo from '../../images/logo.png';
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
