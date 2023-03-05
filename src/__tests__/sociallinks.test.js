6349ccac2a324746bd36201ac5f64a956a8a4fc9
NatanimA <Natanimabesha@gmail.com>
2023-03-05 16:21:57 +0300

    removed unused imports

--- src/__tests__/sociallinks.test.js
+++ src/__tests__/sociallinks.test.js
@@ -2,10 +2,6 @@ import renderer from 'react-test-renderer'
 
 import { BrowserRouter } from 'react-router-dom'
 
-import userEvent from '@testing-library/user-event'
-
-import { render, screen } from '@testing-library/react';
-
 import SocialLinks from '../Components/Navbar/SocialLinks'
 
 describe("Tests Navbar component",() => {
