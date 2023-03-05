83b2e1e8190cab5f36d92a2e5b565ad92c139491
NatanimA <Natanimabesha@gmail.com>
2023-03-05 16:18:34 +0300

    Insert navlink snapshot tests

---
+++ src/__tests__/navlink.test.js
@@ -0,0 +1,18 @@
+import renderer from 'react-test-renderer'
+
+import { BrowserRouter } from 'react-router-dom'
+
+import userEvent from '@testing-library/user-event'
+
+import { render, screen } from '@testing-library/react';
+
+import NavLinks from '../Components/Navbar/NavLinks'
+
+describe("Tests Navbar component",() => {
+    it("Should render Navbar Component",()=>{
+        const tree = renderer.create(<BrowserRouter>
+            <NavLinks />
+        </BrowserRouter>).toJSON()
+        expect(tree).toMatchSnapshot()
+    })
+})
