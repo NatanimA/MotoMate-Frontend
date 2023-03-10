d1761e38eae99d2bef30a677dccc055ff1a9d15c
NatanimA <Natanimabesha@gmail.com>
2023-03-10 11:59:25 +0300

    Nav test

--- src/__tests__/navlink.test.js
+++ src/__tests__/navlink.test.js
@@ -4,6 +4,10 @@ import { BrowserRouter } from 'react-router-dom'
 
 import NavLinks from '../Components/Navbar/NavLinks'
 
+import userEvent from '@testing-library/user-event';
+// eslint-disable-next-line
+import { render, screen } from '@testing-library/react';
+
 describe("Tests Navbar component",() => {
     it("Should render Navbar Component",()=>{
         const tree = renderer.create(<BrowserRouter>
@@ -11,4 +15,12 @@ describe("Tests Navbar component",() => {
         </BrowserRouter>).toJSON()
         expect(tree).toMatchSnapshot()
     })
+
+    const list = screen.getByRole('list',{
+        hidden:true
+    }).childElementCount;
+    expect(list).toEqual(5);
+    const link = screen.getByText('Motorcycle');
+    userEvent.click(link);
+    expect(link.closest('a')).toHaveAttribute('class', 'active');
 })
